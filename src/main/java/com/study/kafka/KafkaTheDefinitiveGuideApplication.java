96370ef34ba394b8a5b62acd5463c76d707711fa
daisy6365 <gksekqls9808@inu.ac.kr>
2025-08-07 17:47:19 +0900

    init: project 생성

---
+++ src/main/java/com/study/kafka/KafkaTheDefinitiveGuideApplication.java
@@ -0,0 +1,13 @@
+package com.study.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTheDefinitiveGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaTheDefinitiveGuideApplication.class, args);
+    }
+
+}
