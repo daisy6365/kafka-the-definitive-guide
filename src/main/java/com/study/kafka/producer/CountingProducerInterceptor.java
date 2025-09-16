0b78792b5589406d0b2d50e1d4c60ae4e70016a7
daisy6365 <gksekqls9808@inu.ac.kr>
2025-09-16 09:19:42 +0900

    add: 주석

--- src/main/java/com/study/kafka/producer/CountingProducerInterceptor.java
+++ src/main/java/com/study/kafka/producer/CountingProducerInterceptor.java
@@ -1,5 +1,6 @@
 package com.study.kafka.producer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerInterceptor;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -14,6 +15,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * Producer Interceptor 예제
  * 전송된 message의 수와 특정한 시간 윈도우 사이에 broker가 리턴한 ack수 집계 class
  */
+@Slf4j
 public class CountingProducerInterceptor implements ProducerInterceptor {
     ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     static AtomicLong numSent = new AtomicLong(0);
@@ -25,6 +27,7 @@ public class CountingProducerInterceptor implements ProducerInterceptor {
      */
     @Override
     public ProducerRecord onSend(ProducerRecord record) {
+        // 전송에 대한 카운팅
         numSent.incrementAndGet();
         return record;
     }
@@ -35,6 +38,7 @@ public class CountingProducerInterceptor implements ProducerInterceptor {
      */
     @Override
     public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
+        // 응답에 대한 카운팅
         numAcked.incrementAndGet();
     }
 
@@ -43,15 +47,24 @@ public class CountingProducerInterceptor implements ProducerInterceptor {
         executorService.shutdown();
     }
 
+    /**
+     * class 초기화 시 설정값을 읽어옴
+     * 주기마다 run()을 자동 실행하도록 스케줄러에 등록
+     */
     @Override
     public void configure(Map<String, ?> configs) {
-        Long windowSize = Long.valueOf(
-                (String) configs.get("counting.interceptor.window.size.ms"));
+//        Long windowSize = Long.valueOf(
+//                (String) configs.get("counting.interceptor.window.size.ms"));
+        // 형변환에 취약하므로 안전하게 구현 -> instanceof
+        Object raw = configs.get("counting.interceptor.window.size.ms");
+        long windowSize = (raw instanceof Number)
+                ? ((Number) raw).longValue()
+                : Long.parseLong(String.valueOf(raw));
         executorService.scheduleAtFixedRate(CountingProducerInterceptor::run, windowSize, windowSize, TimeUnit.MILLISECONDS);
     }
 
     public static void run() {
-        System.out.println(numSent);
-        System.out.println(numAcked);
+        log.info("CountingProducerInterceptor, numSent = {}", numSent);
+        log.info("CountingProducerInterceptor, numAcked = {}", numAcked);
     }
 }
