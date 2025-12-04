e424b8f7727a19fff2757f4f0e3aa9e0a9161fa0
daisy6365 <gksekqls9808@inu.ac.kr>
2025-12-04 15:46:17 +0900

    add: 예외처리 기능 추가

--- src/main/java/com/study/kafka/common/exception/ErrorCode.java
+++ src/main/java/com/study/kafka/common/exception/ErrorCode.java
@@ -1,4 +1,4 @@
-package com.study.kafka.exception;
+package com.study.kafka.common.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -6,18 +6,17 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    // 멱등성 001~099
     IDEMPOTENCY_VIOLATION("001", "Duplicate event received"),
 
-    // 계좌 101~199
     ACCOUNT_NOT_FOUND("101", "Account not found"),
+    INSUFFICIENT_BALANCE("102", "Insufficient balance"),
+    INVALID_TRANSACTION_TYPE("103", "Unsupported transaction type"),
+    INVALID_AMOUNT("104", "Amount must be greater than zero"),
 
-    // 정책 201~299
     POLICY_NOT_FOUND("201", "Alert policy not registered"),
     POLICY_THRESHOLD_NOT_MET("202", "Threshold amount condition not met"),
     POLICY_TIME_NOT_ALLOWED("203", "Notification not allowed in current time window"),
 
-    // 연락정보
     PREF_NOT_FOUND("301", "Alert preference not found");
 
     private final String code;
